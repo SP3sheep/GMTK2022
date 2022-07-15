cb9449063842bf2b81a33bf99aaab7c25b497935
EmilUpp <emil.sitell@gmail.com>
2022-07-16 00:47:09 +0200

    Health Bar

--- Assets/BulletScript.cs
+++ Assets/BulletScript.cs
@@ -7,11 +7,13 @@ public class BulletScript : MonoBehaviour
 
     public float speed;
     public int die;
+    public Sprite[] sprites;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        die = GameObject.FindGameObjectWithTag("DiceScroller").GetComponent<DiceScroller>().value + 1;
+        gameObject.GetComponent<SpriteRenderer>().sprite = sprites[die - 1];
     }
 
     // Update is called once per frame
@@ -27,7 +29,7 @@ public class BulletScript : MonoBehaviour
             collider.gameObject.GetComponent<EnemyScript>().TakeDamage();
         }
 
-        if (collider.gameObject.tag != "Bullet")
+        if (collider.gameObject.tag != "Bullet" && collider.gameObject.tag != "Player")
         {
             Destroy(gameObject);
         }
