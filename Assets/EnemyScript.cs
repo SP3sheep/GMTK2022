b8131cab06d7aef7bca6e29158d4cc2d262e0b35
Pontus Forsmark <pontus.forsmarks@gmail.com>
2022-07-16 00:20:00 +0200

    Enemies now use rigidbody forces and repel each other

--- Assets/EnemyScript.cs
+++ Assets/EnemyScript.cs
@@ -11,24 +11,38 @@ public class EnemyScript : MonoBehaviour
 
     public float speed;
     public Sprite[] sprites;
+    public LayerMask enemiesLayer;
 
     GameObject player;
+    Rigidbody2D rb;
 
 
     // Start is called before the first frame update
     void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
+
         currentDie = Random.Range(1, maxValue);
         player = GameObject.FindGameObjectWithTag("Player");
 
-        gameObject.GetComponent<SpriteRenderer>().sprite = sprites[currentDie - 1];
+        GetComponent<SpriteRenderer>().sprite = sprites[currentDie - 1];
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         // Move towards player
-        transform.position += (player.transform.position - transform.position).normalized * speed * Time.deltaTime;
+        rb.AddForce((player.transform.position - transform.position).normalized * speed * Time.deltaTime);
+
+        float distToPlayer = Vector2.Distance(transform.position, player.transform.position);
+        if(distToPlayer > 1)
+        {
+            Collider2D[] nearbyEnemies = Physics2D.OverlapCircleAll(transform.position, 1, enemiesLayer);
+            foreach (Collider2D e in nearbyEnemies)
+            {
+                rb.AddForce((transform.position - e.transform.position).normalized * 2f);
+            }
+        }
     }
 
     public void TakeDamage()
